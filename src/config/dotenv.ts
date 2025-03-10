f98a75bdd1590746d9fd50b7a14e9bdc984360ec
razzodev <razzo.des@gmail.com>
2025-03-10 18:02:05 +0700

    fix(env vars): handle exports

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,12 +1,15 @@
 import * as dotenv from 'dotenv';
 
-const isProduction = process.env.NODE_ENV === 'production';
+export default function loadEnvs() {
+    const isProduction = process.env.NODE_ENV === 'production';
 
-// Load environment variables based on environment
-if (isProduction) {
-    dotenv.config({ path: '.env.production' }); // Load .env.production
-    console.log('Running in production mode.');
-} else {
-    dotenv.config({ path: '.env.local' }); // Load .env.local
-    console.log('Running in development mode.');
+    // Load environment variables based on environment
+    if (isProduction) {
+        dotenv.config({ path: '.env.production' }); // Load .env.production
+        console.log('Running in production mode.');
+    } else {
+        dotenv.config({ path: '.env.local' }); // Load .env.local
+        console.log('Running in development mode.');
+    }
 }
+
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,8 @@
-import './dotenv'
+import loadEnvs from './dotenv'
+import getCloudflareConfig from './cloudflare'
+
+loadEnvs()
+export default {
+    loadEnvs,
+    cf: getCloudflareConfig(),
+}
