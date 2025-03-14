30789df46b86cb98088ab7aea97fa19845b3ecf7
razzodev <razzo.des@gmail.com>
2025-03-14 13:21:08 +0700

    apply cache or execution to mongodb methods

--- src/features/databases/mongodb/mongodb.service.ts
+++ src/features/databases/mongodb/mongodb.service.ts
@@ -1,5 +1,5 @@
 import { Db, MongoClient, Collection, InsertOneResult, InsertManyResult, DeleteResult, UpdateResult } from 'mongodb';
-import { InMemoryCacheService } from '../../../services/cache';
+import { InMemoryCacheService, getCachedOrExecute } from '../../../services/cache';
 
 export class MongoDatabaseService {
     private cacheService: InMemoryCacheService;
@@ -15,6 +15,8 @@ export class MongoDatabaseService {
     }
 
     deleteDatabase = async (dbName: string): Promise<void> => {
+        const keysToDelete = this.cacheService.getKeys().filter(key => key.startsWith(`${dbName}:`));
+        this.cacheService.del(keysToDelete);
         await this.getDatabase(dbName).dropDatabase();
     }
 
@@ -23,34 +25,48 @@ export class MongoDatabaseService {
     }
 
     deleteCollection = async (dbName: string, collectionName: string): Promise<void> => {
+        const keysToDelete = this.cacheService.getKeys().filter(key => key.includes(`${dbName}:${collectionName}`));
+        this.cacheService.del(keysToDelete);
         await this.getDatabase(dbName).collection(collectionName).drop();
     }
 
     find = async (dbName: string, collection: string, query: any): Promise<any[]> => {
-        return this.getDatabase(dbName).collection(collection).find(query).toArray();
+        const cacheKey = `find:${dbName}:${collection}:${JSON.stringify(query)}`;
+        return getCachedOrExecute(this.cacheService, cacheKey, () =>
+            this.getDatabase(dbName).collection(collection).find(query).toArray());
     }
 
     findAll = async (dbName: string, collection: string): Promise<any[]> => {
-        return this.getDatabase(dbName).collection(collection).find({}).toArray();
+        const cacheKey = `findAll:${dbName}:${collection}`;
+        return getCachedOrExecute(this.cacheService, cacheKey, () =>
+            this.getDatabase(dbName).collection(collection).find({}).toArray()
+        );
     }
 
     findOne = async (dbName: string, collection: string, query: any): Promise<any> => {
-        return this.getDatabase(dbName).collection(collection).findOne(query);
+        const cacheKey = `findOne:${dbName}:${collection}:${JSON.stringify(query)}`;
+        return getCachedOrExecute(this.cacheService, cacheKey, () =>
+            this.getDatabase(dbName).collection(collection).findOne(query)
+        );
     }
 
     insertOne = async (dbName: string, collection: string, document: any): Promise<InsertOneResult> => {
+        this.cacheService.del([`findAll:${dbName}:${collection}`]);
         return this.getDatabase(dbName).collection(collection).insertOne(document);
     }
 
     insertMany = async (dbName: string, collection: string, documents: any[]): Promise<InsertManyResult> => {
+        this.cacheService.del([`findAll:${dbName}:${collection}`]);
         return this.getDatabase(dbName).collection(collection).insertMany(documents);
     }
 
     updateOne = async (dbName: string, collection: string, filter: any, update: any): Promise<UpdateResult> => {
+        this.cacheService.del([`findAll:${dbName}:${collection}`, `findOne:${dbName}:${collection}:${JSON.stringify(filter)}`]);
         return this.getDatabase(dbName).collection(collection).updateOne(filter, update);
     }
 
     deleteOne = async (dbName: string, collection: string, filter: any): Promise<DeleteResult> => {
+        this.cacheService.del([`findAll:${dbName}:${collection}`, `findOne:${dbName}:${collection}:${JSON.stringify(filter)}`]);
         return this.getDatabase(dbName).collection(collection).deleteOne(filter);
     }
 }
